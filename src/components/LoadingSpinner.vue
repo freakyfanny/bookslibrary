c25aefe52c0446327bf5aa8e24de65b92e644bc1
Fanny Maras <fannypeterssons@icloud.com>
2023-10-04 13:14:55 +0200

    spinner styling

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -2,11 +2,11 @@
   <div class="flex items-start justify-center min-h-screen">
     <div class="relative">
       <div
-        class="w-16 h-16 rounded-full absolute border-2 border-dashed border-gray-200"
+        class="w-16 h-16 rounded-full absolute border-4 border-solid border-blue-200"
       ></div>
 
       <div
-        class="w-16 h-16 rounded-full animate-spin absolute border-2 border-dashed border-blue-600 border-t-transparent"
+        class="w-16 h-16 rounded-full animate-spin absolute border-4 border-solid border-blue-500 border-t-transparent"
       ></div>
     </div>
   </div>
